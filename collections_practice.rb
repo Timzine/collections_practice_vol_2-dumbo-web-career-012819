8a1cd42ab99b6a8d5aaee4a31096386c272208bf
Lemrabt Boudadya <lboudadya@gmail.com>
2019-01-09 18:07:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,13 @@
+collection = ["ruby", "rspec", "rails"]
 # your code goes here
+def begins_with_r(array)
+  i = 0 
+  while i < array.length 
+   if array[i].start_with?("r")
+     puts "true"
+  else puts "false"
+    i = i + 1 
+  end
+end
+  
+end 
